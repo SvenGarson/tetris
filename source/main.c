bf0048f4929e27fd711a45ea806fc792454c1721
SvenGarson <sven.garson@outlook.com>
2025-04-20 18:24:54 +0200

    Get basic scaled rendering to work

--- source/main.c
+++ source/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 #include <SDL3/SDL.h>
 
 // Helpers - Arguments
@@ -23,6 +24,27 @@ const char * help_args_key_value_first(int argc, char * argv[], const char * key
    return NULL;
 }
 
+// Helpers - Colors
+typedef uint32_t color_rgba_t;
+
+color_rgba_t color_rgba_make_rgba(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha)
+{
+   color_rgba_t color = 0x00;
+
+   color |= red;
+   color <<= 8;
+
+   color |= green;
+   color <<= 8;
+
+   color |= blue;
+   color <<= 8;
+
+   color |= alpha;
+
+   return color;
+}
+
 // Constants
 const char * ARG_KEY_DIR_ABS_RES = "-abs_res_dir";
 
@@ -37,9 +59,132 @@ int main(int argc, char * argv[])
       return EXIT_FAILURE;
    }
 
+   // Create SDL window
+   SDL_Window * sdl_window = SDL_CreateWindow("Tetris", 0, 0, SDL_WINDOW_FULLSCREEN);
+   if (NULL == sdl_window)
+   {
+      printf("\nFailed to create SDL window - Error: %s", SDL_GetError());
+      return EXIT_FAILURE;
+   }
+
+   // Create SDL renderer
+   SDL_Renderer * sdl_renderer = SDL_CreateRenderer(sdl_window, NULL);
+   if (NULL == sdl_renderer)
+   {
+      printf("\nFailed to create SDL renderer - Error: %s", SDL_GetError());
+      return EXIT_FAILURE;
+   }
+
+   // Create offline rendering texture
+   const int VIRTUAL_WIDTH = 15;
+   const int VIRTUAL_HEIGHT = 10;
+   const int VIRTUAL_PIXEL_COUNT = VIRTUAL_WIDTH * VIRTUAL_HEIGHT;
+   color_rgba_t * offline_pixels = malloc(sizeof(color_rgba_t) * VIRTUAL_PIXEL_COUNT);
+   if (NULL == offline_pixels)
+   {
+      printf("\nFailed to allocate [%d] offline pixels", VIRTUAL_PIXEL_COUNT);
+      return EXIT_FAILURE;
+   }
+
+   // Create online rendering texture
+   SDL_Texture * sdl_texture_online = SDL_CreateTexture(
+      sdl_renderer,
+      SDL_PIXELFORMAT_RGBA8888,
+      SDL_TEXTUREACCESS_STREAMING,
+      VIRTUAL_WIDTH,
+      VIRTUAL_HEIGHT
+   );
+   if (NULL == sdl_texture_online)
+   {
+      printf("\nFailed to create sdl online texture - Error: %s", SDL_GetError());
+      return EXIT_FAILURE;
+   }
+
+   // Set online texture filtering
+   const bool SUCCESS_TEXTURE_NEAREST = SDL_SetTextureScaleMode(sdl_texture_online, SDL_SCALEMODE_NEAREST);
+   if (!SUCCESS_TEXTURE_NEAREST)
+   {
+      printf("\nFailed to set online texture filtering to nearest - Error: %s", SDL_GetError());
+      return EXIT_FAILURE;
+   }
+
    // Log engine status
    printf("\n\nEngine Information");
    printf("\n\t%-15s: %s", "resource directory", DIR_ABS_RES);
+
+   // Game loop
+   bool tetris_close_requested = false;
+   while (false == tetris_close_requested)
+   {
+      // Consume window events
+      SDL_Event window_event;
+      while (SDL_PollEvent(&window_event))
+      {
+         if (SDL_EVENT_KEY_DOWN == window_event.type && SDL_SCANCODE_ESCAPE == window_event.key.scancode)
+         {
+            tetris_close_requested = true;
+         }
+      }
+
+      // Render into offline texture
+      for (int i = 0; i < VIRTUAL_PIXEL_COUNT; ++i)
+      {
+         offline_pixels[i] = color_rgba_make_rgba(rand() % 256, rand() % 256, rand() % 256, 0xFF);
+      }
+
+      // Copy offline to online texture
+      const bool SUCCESS_UPDATE_TEXTURE = SDL_UpdateTexture(
+         sdl_texture_online,
+         NULL,
+         offline_pixels,
+         sizeof(color_rgba_t) * VIRTUAL_WIDTH
+      );
+
+      // Clear backbuffer
+      SDL_SetRenderDrawColor(sdl_renderer, 50, 150, 250, 0xFF);
+      const bool SUCCESS_BACKBUFFER_CLEAR = SDL_RenderClear(sdl_renderer);
+      if (!SUCCESS_BACKBUFFER_CLEAR)
+      {
+         printf("\nFailed to clear backbuffer - Error: %s", SDL_GetError());
+         break;
+      }
+
+      // Render scaled virtual texture
+      const int SCALE = 50;
+      SDL_FRect dest;
+      dest.x = 0;
+      dest.y = 0;
+      dest.w = VIRTUAL_WIDTH * SCALE;
+      dest.h = VIRTUAL_HEIGHT * SCALE;
+      const bool SUCCESS_RENDER_TEXTURE = SDL_RenderTexture(
+         sdl_renderer,
+         sdl_texture_online,
+         NULL,
+         &dest
+      );
+      if (!SUCCESS_RENDER_TEXTURE)
+      {
+         printf("\nFailed to render online texture - Error: %s", SDL_GetError());
+         break;
+      }
+
+      // Swap buffers
+      const bool SUCCESS_RENDER = SDL_RenderPresent(sdl_renderer);
+      if (!SUCCESS_RENDER)
+      {
+         printf("\nFailed to render - Error: %s", SDL_GetError());
+         break;
+      }
+   }
+
+   // Cleanup custom
+   free(offline_pixels);
+
+   // Cleanup SDL
+   SDL_DestroyTexture(sdl_texture_online);
+   SDL_DestroyWindow(sdl_window);
+   SDL_DestroyRenderer(sdl_renderer);
+   SDL_Quit();
 
    // Back to OS
    return EXIT_SUCCESS;
